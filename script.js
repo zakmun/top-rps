5ab47a7170eda8a8307f26319959c4d551633ce9
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-12-02 20:26:35 +0000

    Initiliaze rock, paper & scissors variable for CPU

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 const r = "rock";
 const p = "paper";
 const s = "Scissors"
-
-function getComputerChoice(rps) {
-
-    randrps = Math.floor(Math.random() * rps)
-
-    if (randrps == 0) {
-        return (r);
-    } else if (rps == 1) {
-        return (p);
-    } else {
-        return (s);
-    }
-
-    
-}
-
-console.log(getComputerChoice(3))
