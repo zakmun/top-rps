ab2dcbb04d80447a0f4faab9957e23411530d86a
Zaki Munshi <zaki-munshi@hotmail.co.uk>
2024-12-02 20:24:12 +0000

    Initialise variables for rock, paper & scissors

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+const r = "rock";
+const p = "paper";
+const s = "Scissors"
+
+function getComputerChoice(rps) {
+
+    randrps = Math.floor(Math.random() * rps)
+
+    if (randrps == 0) {
+        return (r);
+    } else if (rps == 1) {
+        return (p);
+    } else {
+        return (s);
+    }
+
+    
+}
+
+console.log(getComputerChoice(3))
